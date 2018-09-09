534b0fcdc1765393c4999c7bd75afdaddf09cbb4
SirKratosAurion <v_gamefreak@yahoo.com>
2018-09-09 16:19:44 -0400

    Initial Push
    Initial Push to add script to github.

---
+++ IMVUAffinity.ps1
@@ -0,0 +1,10 @@
+﻿$app_name = "$ENV:APPDATA\IMVUClient\IMVUClient.exe"
+$app_arguments = "arg0"
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $app_name
+$pinfo.Arguments = $app_arguments
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start()
+$p.ProcessorAffinity=0x3
